b3b39b8402f1e0f21ff89ccbca4881c69b445c8a
Anastasiia Tsapko <anastsapko@gmail.com>
2024-03-15 19:56:01 +0200

    Created function for getting places

--- functions/main.py
+++ functions/main.py
@@ -1,18 +1,45 @@
+import json
 from firebase_functions.firestore_fn import (
     on_document_created,
     Event,
     DocumentSnapshot,
 )
-from firebase_admin import initialize_app, firestore
-import google.cloud.firestore
+from firebase_admin import initialize_app
 import math
-import googlemaps
+from decouple import config
+import requests
 
 initialize_app()
 
-API_KEY = open("API_KEY.txt", "r").read()
+API_KEY = config("API_KEY")
+url = "https://places.googleapis.com/v1/places:searchNearby"
 
 
 @on_document_created(document="cities/{cityId}")
 def createPlaces(event: Event[DocumentSnapshot]) -> None:
     radius = (math.sqrt(event.data.get("area") / math.pi)) * 1000
+
+    parameters = {
+        "includedTypes": ["tourist_attraction"],
+        "maxResultCount": 10,
+        "locationRestriction": {
+            "circle": {
+                "center": {
+                    "latitude": event.data.get("latitude"),
+                    "longitude": event.data.get("longitude"),
+                },
+                "radius": radius,
+            }
+        },
+        "languageCode": "en",
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": API_KEY,
+        "X-Goog-FieldMask": "places.displayName",
+    }
+
+    post_response = requests.post(url, json=parameters, headers=headers)
+
+    print(post_response.content)
