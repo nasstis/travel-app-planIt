81d8ec834b28d75778cc06411950e142c4237282
Anastasiia Tsapko <anastsapko@gmail.com>
2024-03-15 20:52:40 +0200

    Created function to get places on js

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,42 @@
-
+require("dotenv").config();
 const { onDocumentCreated } = require("firebase-functions/v2/firestore");
 const { initializeApp } = require("firebase-admin/app");
 
 initializeApp();
 
-exports.myfunction = onDocumentCreated("cities/{cityId}", (event) => {
-    console.log("Document data:", event.data.data().cityId);
+API_KEY = process.env.API_KEY
+url = "https://places.googleapis.com/v1/places:searchNearby"
+
+exports.myfunction = onDocumentCreated("cities/{cityId}", async (event) => {
+    const radius = (Math.sqrt(event.data.data().area / Math.PI)) * 1000;
+    const parameters = {
+        includedTypes: ["tourist_attraction"],
+        maxResultCount: 10,
+        locationRestriction: {
+            circle: {
+                center: {
+                    latitude: event.data.data().latitude,
+                    longitude: event.data.data().longitude
+                },
+                radius: radius
+            }
+        },
+        languageCode: "en"
+    };
+
+    const headers = {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": API_KEY,
+        "X-Goog-FieldMask": "places.displayName"
+    };
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(parameters)
+    });
+
+    const data = await response.json()
+
+    console.log(JSON.stringify(data));
 });
