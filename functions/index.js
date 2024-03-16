884b8ecf485ff2ac5853c058e843bec646cb2c77
Anastasiia Tsapko <anastsapko@gmail.com>
2024-03-16 12:37:27 +0200

    Added function to save places in Cloud Firestore

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { onDocumentCreated } = require("firebase-functions/v2/firestore");
 const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
 initializeApp();
 
@@ -27,7 +28,7 @@ exports.myfunction = onDocumentCreated("cities/{cityId}", async (event) => {
     const headers = {
         "Content-Type": "application/json",
         "X-Goog-Api-Key": API_KEY,
-        "X-Goog-FieldMask": "places.displayName"
+        "X-Goog-FieldMask": "places.displayName,places.id,places.location,places.photos,places.shortFormattedAddress,places.rating,places.regularOpeningHours,places.userRatingCount,places.reviews,places.restroom,places.goodForChildren,places.editorialSummary,places.types,places.businessStatus"
     };
 
     const response = await fetch(url, {
@@ -38,5 +39,35 @@ exports.myfunction = onDocumentCreated("cities/{cityId}", async (event) => {
 
     const data = await response.json()
 
-    console.log(JSON.stringify(data));
+    processData(data.places);
 });
+
+async function processData(places) {
+    for (const place of places) {
+        const placeMap = {
+            "name": place.displayName.text,
+            "id": place.id,
+            "types": place.types,
+            "latitude": place.location.latitude,
+            "longitude": place.location.longitude,
+            "rating": place.rating,
+            "businessStatus": place.businessStatus,
+            "address": place.shortFormattedAddress,
+            "description": place.editorialSummary.text,
+            "reviews": place.reviews,
+            "photos": place.photos,
+        };
+
+        if (place.goodForChildren !== undefined) {
+            placeMap.goodForChildren = place.goodForChildren;
+        }
+        if (place.regularOpeningHours !== undefined) {
+            placeMap.regularOpeningHours = place.regularOpeningHours.weekdayDescriptions;
+        }
+        if (place.restroom !== undefined) {
+            placeMap.restroom = place.restroom;
+        }
+
+        await getFirestore().collection("places").doc(placeMap.id).set(placeMap);
+    }
+}
