6d020064a7d2eb07eb4d55bddd87f65080a0669b
Anastasiia Tsapko <anastsapko@gmail.com>
2024-03-19 11:52:16 +0200

    Refactored cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,106 +1,135 @@
+/* eslint-disable max-len, require-jsdoc */
 require("dotenv").config();
-const { onDocumentCreated } = require("firebase-functions/v2/firestore");
-const { initializeApp } = require("firebase-admin/app");
-const { getFirestore } = require("firebase-admin/firestore");
-const { getStorage, getDownloadURL } = require("firebase-admin/storage");
+const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+const {getStorage, getDownloadURL} = require("firebase-admin/storage");
 
 initializeApp();
 
-API_KEY = process.env.API_KEY
-url = "https://places.googleapis.com/v1/places:searchNearby"
+const API_KEY = process.env.API_KEY;
+const url = "https://places.googleapis.com/v1/places:searchNearby";
 
-exports.myfunction = onDocumentCreated("cities/{cityId}", async (event) => {
-    const radius = (Math.sqrt(event.data.data().area / Math.PI)) * 1000;
-    const parameters = {
+const db = getFirestore();
+
+exports.myfunction = onDocumentCreated(
+    {
+      document: "cities/{cityId}",
+      region: "europe-west1",
+      timeoutSeconds: 300,
+      memory: "512MiB",
+    }, async (event) => {
+      const radius = Math.sqrt(event.data.data().area / Math.PI) * 1000;
+      const parameters = {
         includedTypes: ["tourist_attraction"],
         maxResultCount: 10,
         locationRestriction: {
-            circle: {
-                center: {
-                    latitude: event.data.data().latitude,
-                    longitude: event.data.data().longitude
-                },
-                radius: radius
-            }
+          circle: {
+            center: {
+              latitude: event.data.data().latitude,
+              longitude: event.data.data().longitude,
+            },
+            radius: radius,
+          },
         },
-        languageCode: "en"
-    };
-    const headers = {
+        languageCode: "en",
+      };
+      const headers = {
         "Content-Type": "application/json",
         "X-Goog-Api-Key": API_KEY,
-        "X-Goog-FieldMask": "places.displayName,places.id,places.location,places.photos,places.shortFormattedAddress,places.rating,places.regularOpeningHours,places.userRatingCount,places.reviews,places.restroom,places.goodForChildren,places.editorialSummary,places.types,places.businessStatus"
-    };
+        "X-Goog-FieldMask": "places.displayName,places.id,places.location,places.photos,places.shortFormattedAddress,places.rating,places.regularOpeningHours,places.userRatingCount,places.reviews,places.restroom,places.goodForChildren,places.editorialSummary,places.types,places.businessStatus",
+      };
 
-    const response = await fetch(url, {
-        method: 'POST',
+      const response = await fetch(url, {
+        method: "POST",
         headers: headers,
-        body: JSON.stringify(parameters)
+        body: JSON.stringify(parameters),
+      });
+
+      const data = await response.json();
+      const batch = db.batch();
+      await processData(data.places, event.data.data().cityId, batch);
+      await batch.commit();
     });
 
-    const data = await response.json();
+async function processData(places, cityId, batch) {
+  const placePromises = await places.map(async (place) => {
+    const photoUrls = await getPhotosUrl(place.photos);
 
-    processData(data.places);
-});
+    console.log("processData");
+    const placeMap = {
+      name: place.displayName.text,
+      id: place.id,
+      types: place.types,
+      latitude: place.location.latitude,
+      longitude: place.location.longitude,
+      address: place.shortFormattedAddress,
+      reviews: place.reviews,
+      photos: photoUrls,
+      cityId: cityId,
+    };
 
-async function processData(places) {
-    for (const place of places) {
-        const photoUrls = await getPhotosUrl(place.photos);
-
-        const placeMap = {
-            "name": place.displayName.text,
-            "id": place.id,
-            "types": place.types,
-            "latitude": place.location.latitude,
-            "longitude": place.location.longitude,
-            "rating": place.rating,
-            "businessStatus": place.businessStatus,
-            "address": place.shortFormattedAddress,
-            "description": place.editorialSummary.text,
-            "reviews": place.reviews,
-            "photos": photoUrls,
-        };
-
-        if (place.goodForChildren !== undefined) {
-            placeMap.goodForChildren = place.goodForChildren;
-        }
-        if (place.regularOpeningHours !== undefined) {
-            placeMap.regularOpeningHours = place.regularOpeningHours.weekdayDescriptions;
-        }
-        if (place.restroom !== undefined) {
-            placeMap.restroom = place.restroom;
-        }
-
-        await getFirestore().collection("places").doc(placeMap.id).set(placeMap);
+    if (place.editorialSummary) {
+      placeMap.description = place.editorialSummary.text;
     }
+    if (place.businessStatus) {
+      placeMap.businessStatus = place.businessStatus;
+    }
+    if (place.rating) {
+      placeMap.rating = place.rating;
+    }
+    if (place.goodForChildren) {
+      placeMap.goodForChildren = place.goodForChildren;
+    }
+    if (place.regularOpeningHours) {
+      placeMap.regularOpeningHours = place.regularOpeningHours.weekdayDescriptions;
+    }
+    if (place.restroom) {
+      placeMap.restroom = place.restroom;
+    }
+
+
+    const placeRef = db.collection("places").doc(placeMap.id);
+    batch.set(placeRef, placeMap);
+  });
+  await Promise.all(placePromises);
 }
 
 async function getPhotosUrl(photos) {
-    const urls = [];
-    for (const photo of photos) {
-        try {
-            const get_photo_url = `https://places.googleapis.com/v1/${photo.name}/media?maxHeightPx=${photo.heightPx}&maxWidthPx=${photo.widthPx}&key=${API_KEY}`;
+  console.log("getPhotourls");
+  try {
+    const urls = await photos.map(async (photo) => {
+      const getPhotoUrl = `https://places.googleapis.com/v1/${photo.name}/media?maxHeightPx=${photo.heightPx}&maxWidthPx=${photo.widthPx}&key=${API_KEY}`;
 
-            const headers = {
-                "Content-Type": "application/json",
-                "X-Goog-Api-Key": API_KEY,
-            };
+      const headers = {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": API_KEY,
+      };
 
-            const response = await fetch(get_photo_url, {
-                method: 'GET',
-                headers: headers,
-            });
+      const response = await fetch(getPhotoUrl, {
+        method: "GET",
+        headers: headers,
+      });
 
-            const file = await response.arrayBuffer();
-            var uint8file = new Uint8Array(file);
+      const file = await response.arrayBuffer();
+      const uint8file = new Uint8Array(file);
 
-            const fileRef = getStorage().bucket().file(`${photo.name}.jpg`);
-            await fileRef.save(uint8file, { resumable: false, metadata: { contentType: "image/jpg" } });
+      if (file.byteLength < 1024) {
+        return;
+      }
 
-            const photoUrl = await getDownloadURL(fileRef);
-            urls.push(photoUrl);
-        } catch (error) {
-            console.log("PHOTO ERROR", error);
-        }
-    }
-    return urls;
+      const fileRef = getStorage().bucket().file(`${photo.name}.jpg`);
+      await fileRef.save(uint8file, {resumable: false, metadata: {contentType: "image/jpg"}});
+
+      const photoUrl = await getDownloadURL(fileRef);
+      return photoUrl;
+    });
+
+    const urlsData = await Promise.all(urls);
+    const filteredUrls = urlsData.filter(Boolean);
+
+    return filteredUrls;
+  } catch (error) {
+    console.log("PHOTO ERROR", error);
+  }
 }
