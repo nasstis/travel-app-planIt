d8a2ad23e854fdd90675d209bb8a6d38f7b0a1b6
Anastasiia Tsapko <anastsapko@gmail.com>
2024-04-09 13:30:16 +0300

    Added case if there are no reviews

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 /* eslint-disable max-len, require-jsdoc */
 require("dotenv").config();
-const {onDocumentCreated} = require("firebase-functions/v2/firestore");
-const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
-const {getStorage, getDownloadURL} = require("firebase-admin/storage");
+const { onDocumentCreated } = require("firebase-functions/v2/firestore");
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+const { getStorage, getDownloadURL } = require("firebase-admin/storage");
 
 initializeApp();
 
@@ -13,44 +13,44 @@ const url = "https://places.googleapis.com/v1/places:searchNearby";
 const db = getFirestore();
 
 exports.myfunction = onDocumentCreated(
-    {
-      document: "cities/{cityId}",
-      region: "europe-west1",
-      timeoutSeconds: 540,
-      memory: "512MiB",
-    }, async (event) => {
-      const radius = Math.sqrt(event.data.data().area / Math.PI) * 1000;
-      const parameters = {
-        includedTypes: ["tourist_attraction"],
-        maxResultCount: 10,
-        locationRestriction: {
-          circle: {
-            center: {
-              latitude: event.data.data().latitude,
-              longitude: event.data.data().longitude,
-            },
-            radius: radius,
+  {
+    document: "cities/{cityId}",
+    region: "europe-west1",
+    timeoutSeconds: 540,
+    memory: "512MiB",
+  }, async (event) => {
+    const radius = Math.sqrt(event.data.data().area / Math.PI) * 1000;
+    const parameters = {
+      includedTypes: ["tourist_attraction"],
+      maxResultCount: 10,
+      locationRestriction: {
+        circle: {
+          center: {
+            latitude: event.data.data().latitude,
+            longitude: event.data.data().longitude,
           },
+          radius: radius,
         },
-        languageCode: "en",
-      };
-      const headers = {
-        "Content-Type": "application/json",
-        "X-Goog-Api-Key": API_KEY,
-        "X-Goog-FieldMask": "places.displayName,places.id,places.location,places.photos,places.shortFormattedAddress,places.rating,places.regularOpeningHours,places.userRatingCount,places.reviews,places.restroom,places.goodForChildren,places.editorialSummary,places.types,places.businessStatus",
-      };
+      },
+      languageCode: "en",
+    };
+    const headers = {
+      "Content-Type": "application/json",
+      "X-Goog-Api-Key": API_KEY,
+      "X-Goog-FieldMask": "places.displayName,places.id,places.location,places.photos,places.shortFormattedAddress,places.rating,places.regularOpeningHours,places.userRatingCount,places.reviews,places.restroom,places.goodForChildren,places.editorialSummary,places.types,places.businessStatus",
+    };
 
-      const response = await fetch(url, {
-        method: "POST",
-        headers: headers,
-        body: JSON.stringify(parameters),
-      });
-
-      const data = await response.json();
-      const batch = db.batch();
-      await processData(data.places, event.data.data().cityId, batch);
-      await batch.commit();
+    const response = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(parameters),
     });
+
+    const data = await response.json();
+    const batch = db.batch();
+    await processData(data.places, event.data.data().cityId, batch);
+    await batch.commit();
+  });
 
 async function processData(places, cityId, batch) {
   for await (const place of places) {
@@ -63,11 +63,16 @@ async function processData(places, cityId, batch) {
       latitude: place.location.latitude,
       longitude: place.location.longitude,
       address: place.shortFormattedAddress,
-      reviews: place.reviews,
+      ratingCount: place.userRatingCount,
       photos: photoUrls,
       cityId: cityId,
     };
 
+    if (place.reviews) {
+      placeMap.reviews = place.reviews;
+    } else {
+      placeMap.reviews = [];
+    }
     if (place.editorialSummary) {
       placeMap.description = place.editorialSummary.text;
     }
@@ -117,7 +122,7 @@ async function getPhotosUrl(photos) {
       }
 
       const fileRef = getStorage().bucket().file(`${photo.name}.jpg`);
-      await fileRef.save(uint8file, {resumable: false, metadata: {contentType: "image/jpg"}});
+      await fileRef.save(uint8file, { resumable: false, metadata: { contentType: "image/jpg" } });
 
       const photoUrl = await getDownloadURL(fileRef);
       return photoUrl;
--- lib/screens/place/components/reviews.dart
+++ lib/screens/place/components/reviews.dart
@@ -14,118 +14,124 @@ class Reviews extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 45.0, horizontal: 20.0),
-      child: SingleChildScrollView(
-        physics: const ScrollPhysics(),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: MediaQuery.of(context).size.width * 0.37,
-                child: Column(
-                  children: [
-                    const Text(
-                      'Overall Rating',
-                      style: TextStyle(
-                        fontSize: 17,
-                      ),
-                    ),
-                    Text(
-                      placeRating.toString(),
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 52,
-                      ),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.45,
-                      child: StarDisplay(
-                        rating: placeRating,
-                        size: 26,
-                      ),
-                    ),
-                    const SizedBox(height: 5),
-                    const Text(
-                      'Based on 00 reviews',
-                      style: TextStyle(
-                        fontSize: 15,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              padding: EdgeInsets.zero,
-              itemCount: reviews.length,
-              // shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Column(
-                    children: [
-                      ListTile(
-                        leading: Container(
-                          height: 58,
-                          width: 58,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(100),
-                            image: DecorationImage(
-                                image: CachedNetworkImageProvider(
-                                  reviews[index].authorPhoto,
-                                ),
-                                fit: BoxFit.cover),
-                          ),
-                        ),
-                        title: Text(
-                          reviews[index].authorName,
-                          style: const TextStyle(fontWeight: FontWeight.w500),
-                        ),
-                        subtitle: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            StarDisplay(
-                              rating: reviews[index].rating,
-                              size: 20,
+      child: reviews.isEmpty
+          ? const Center(
+              child: Text('There is no reviews yet...'),
+            )
+          : SingleChildScrollView(
+              physics: const ScrollPhysics(),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.width * 0.37,
+                      child: Column(
+                        children: [
+                          const Text(
+                            'Overall Rating',
+                            style: TextStyle(
+                              fontSize: 17,
                             ),
-                            Text(
-                              reviews[index].publishTime,
-                              style: const TextStyle(fontSize: 12),
+                          ),
+                          Text(
+                            placeRating.toString(),
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 52,
+                            ),
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.45,
+                            child: StarDisplay(
+                              rating: placeRating,
+                              size: 26,
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          const Text(
+                            'Based on 00 reviews',
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    padding: EdgeInsets.zero,
+                    itemCount: reviews.length,
+                    // shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Column(
+                          children: [
+                            ListTile(
+                              leading: Container(
+                                height: 58,
+                                width: 58,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  image: DecorationImage(
+                                      image: CachedNetworkImageProvider(
+                                        reviews[index].authorPhoto,
+                                      ),
+                                      fit: BoxFit.cover),
+                                ),
+                              ),
+                              title: Text(
+                                reviews[index].authorName,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              subtitle: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  StarDisplay(
+                                    rating: reviews[index].rating,
+                                    size: 20,
+                                  ),
+                                  Text(
+                                    reviews[index].publishTime,
+                                    style: const TextStyle(fontSize: 12),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ReadMoreText(
+                                reviews[index].text,
+                                trimMode: TrimMode.Length,
+                                trimLength: 200,
+                                trimCollapsedText: 'See More',
+                                trimExpandedText: ' Hide',
+                                moreStyle: const TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                    color: MyColors.primary),
+                                lessStyle: const TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                    color: MyColors.primary),
+                                style: const TextStyle(fontSize: 15),
+                              ),
                             ),
                           ],
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ReadMoreText(
-                          reviews[index].text,
-                          trimMode: TrimMode.Length,
-                          trimLength: 200,
-                          trimCollapsedText: 'See More',
-                          trimExpandedText: ' Hide',
-                          moreStyle: const TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500,
-                              color: MyColors.primary),
-                          lessStyle: const TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500,
-                              color: MyColors.primary),
-                          style: const TextStyle(fontSize: 15),
-                        ),
-                      ),
-                    ],
+                      );
+                    },
                   ),
-                );
-              },
+                  const SizedBox(
+                    height: 40,
+                  )
+                ],
+              ),
             ),
-            const SizedBox(
-              height: 40,
-            )
-          ],
-        ),
-      ),
     );
   }
 }
