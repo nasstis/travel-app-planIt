69e6b1ecd412b4bb889e808a1bdb9fe39e02390a
Anastasiia Tsapko <anastsapko@gmail.com>
2024-05-12 20:00:24 +0300

    Add a rank to a user's page

--- lib/screens/user/views/user_page.dart
+++ lib/screens/user/views/user_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:travel_app/blocs/auth_bloc/auth_bloc.dart';
 import 'package:travel_app/screens/user/components/profile_button.dart';
+import 'package:travel_app/utils/constants/rank.dart';
 
 class UserPage extends StatelessWidget {
   const UserPage({super.key});
@@ -15,34 +16,101 @@ class UserPage extends StatelessWidget {
         if (state.status == AuthStatus.authenticated) {
           return Column(
             children: [
-              const SizedBox(height: 100),
-              Container(
-                height: MediaQuery.of(context).size.width * 0.28,
-                width: MediaQuery.of(context).size.width * 0.28,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
-                  image: DecorationImage(
-                    image: CachedNetworkImageProvider(state.user!.photo),
-                    fit: BoxFit.cover,
+              const SizedBox(height: 80),
+              Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.25,
                   ),
-                ),
+                  Positioned(
+                    top: 23,
+                    child: Container(
+                      height: MediaQuery.of(context).size.width * 0.28,
+                      width: MediaQuery.of(context).size.width * 0.28,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        image: DecorationImage(
+                          image: CachedNetworkImageProvider(state.user!.photo),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.width * 0.39,
+                    width: MediaQuery.of(context).size.width * 0.39,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                      image: AssetImage(
+                        Rank.frames[0],
+                      ),
+                      fit: BoxFit.cover,
+                    )),
+                  ),
+                  Positioned(
+                    top: 148,
+                    child: Column(
+                      children: [
+                        Text(
+                          state.user!.name,
+                          style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const SizedBox(height: 2),
+                        Text(
+                          state.user!.email,
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 15),
-              Text(
-                state.user!.name,
-                style: const TextStyle(
-                  fontSize: 21,
-                  fontWeight: FontWeight.w600,
-                ),
+              const Divider(),
+              Stack(
+                children: [
+                  Material(
+                    elevation: 8,
+                    borderRadius: BorderRadius.circular(25),
+                    child: Container(
+                      height: 30,
+                      width: MediaQuery.of(context).size.width * 0.43,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        gradient: const RadialGradient(
+                          colors: [
+                            Color(0xFFFFF5E8),
+                            Color(0xFFFFEACD),
+                          ],
+                          radius: 4,
+                          stops: [0.2, 0.9],
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          const SizedBox(width: 50),
+                          Text(
+                            Rank.rankNames[0],
+                            style: const TextStyle(
+                              color: Color(0xF23D3121),
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Image.asset(
+                    Rank.badges[0],
+                    height: 53,
+                  ),
+                ],
               ),
-              const SizedBox(height: 2),
-              Text(
-                state.user!.email,
-                style: const TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              const SizedBox(height: 200),
               const ProfileButton(
                 icon: CupertinoIcons.heart,
                 text: 'Your Favorites',
---
+++ lib/utils/constants/rank.dart
@@ -0,0 +1,49 @@
+class Rank {
+  static const frames = [
+    'assets/images/frames/frame_rank1.png',
+    'assets/images/frames/frame_rank2.png',
+    'assets/images/frames/frame_rank3.png',
+    'assets/images/frames/frame_rank4.png',
+    'assets/images/frames/frame_rank5.png',
+    'assets/images/frames/frame_rank6.png',
+    'assets/images/frames/frame_rank7.png',
+    'assets/images/frames/frame_rank8.png',
+    'assets/images/frames/frame_rank9.png',
+  ];
+
+  static const badges = [
+    'assets/images/badges/badge1.png',
+    'assets/images/badges/badge2.png',
+    'assets/images/badges/badge3.png',
+    'assets/images/badges/badge4.png',
+    'assets/images/badges/badge5.png',
+    'assets/images/badges/badge6.png',
+    'assets/images/badges/badge7.png',
+    'assets/images/badges/badge8.png',
+    'assets/images/badges/badge9.png',
+  ];
+
+  static const ranks = [
+    'assets/images/ranks/rank1.png',
+    'assets/images/ranks/rank2.png',
+    'assets/images/ranks/rank3.png',
+    'assets/images/ranks/rank4.png',
+    'assets/images/ranks/rank5.png',
+    'assets/images/ranks/rank6.png',
+    'assets/images/ranks/rank7.png',
+    'assets/images/ranks/rank8.png',
+    'assets/images/ranks/rank9.png',
+  ];
+
+  static const rankNames = [
+    'Novice Traveler',
+    'Explorer Apprentice',
+    'Journey Enthusiast',
+    'Adventure Seeker',
+    'Travel Connoisseur',
+    'Expedition Expert',
+    'Globetrotting Maestro',
+    'Wanderlust Ambassador',
+    'Nomadic Legend',
+  ];
+}
