634ac0fe7e8d9562c7750f48119f122a89654b3c
Anastasiia Tsapko <anastsapko@gmail.com>
2024-05-13 21:08:07 +0300

    Add gradient animation to background on user's profile page

---
+++ lib/screens/user/components/background_painter.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class BackgroundPainter extends CustomPainter {
+  final Animation<double> animation;
+
+  const BackgroundPainter(this.animation);
+
+  Offset getOffset(Path path) {
+    final pms = path.computeMetrics(forceClosed: false).elementAt(0);
+    final length = pms.length;
+    final offset = pms.getTangentForOffset(length * animation.value)!.position;
+    return offset;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.maskFilter = const MaskFilter.blur(
+      BlurStyle.normal,
+      30,
+    );
+    drawShape1(canvas, size, paint, const Color(0xFF6B51BE));
+    drawShape2(canvas, size, paint, const Color(0xFF9F8DD8));
+    drawShape3(canvas, size, paint, const Color(0xFF4F3C8B));
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+
+  void drawShape1(
+    Canvas canvas,
+    Size size,
+    Paint paint,
+    Color color,
+  ) {
+    paint.color = color;
+    Path path = Path();
+
+    path.moveTo(size.width, 0);
+    path.quadraticBezierTo(
+      size.width,
+      size.height,
+      size.width / 4.5,
+      200,
+    );
+
+    final offset = getOffset(path);
+    canvas.drawCircle(offset, 200, paint);
+  }
+
+  void drawShape2(
+    Canvas canvas,
+    Size size,
+    Paint paint,
+    Color color,
+  ) {
+    paint.color = color;
+    Path path = Path();
+
+    path.moveTo(size.width / 2, 230);
+    path.quadraticBezierTo(
+      size.width / 4.5,
+      size.height / 6,
+      size.width / 2,
+      size.height * 2,
+    );
+
+    final offset = getOffset(path);
+    canvas.drawCircle(offset, 150, paint);
+  }
+
+  void drawShape3(
+    Canvas canvas,
+    Size size,
+    Paint paint,
+    Color color,
+  ) {
+    paint.color = color;
+    Path path = Path();
+
+    path.moveTo(20, 20);
+    path.quadraticBezierTo(
+      size.width * 1.5,
+      size.height * 1.5,
+      size.width / 2,
+      230,
+    );
+
+    final offset = getOffset(path);
+    canvas.drawCircle(offset, 150, paint);
+  }
+}
---
+++ lib/screens/user/components/background_shapes.dart
@@ -0,0 +1,69 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:travel_app/screens/user/components/background_painter.dart';
+
+class BackgroundShapes extends StatefulWidget {
+  const BackgroundShapes({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  State<BackgroundShapes> createState() => _BackgroundShapesState();
+}
+
+class _BackgroundShapesState extends State<BackgroundShapes>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    );
+    _animation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(_controller);
+    _controller.repeat(reverse: true);
+    super.initState();
+  }
+
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Stack(
+        children: [
+          AnimatedBuilder(
+            animation: _animation,
+            builder: (context, child) {
+              return CustomPaint(
+                painter: BackgroundPainter(_animation),
+                child: Container(),
+              );
+            },
+          ),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 60, sigmaY: 60),
+            child: Container(
+              color: Colors.black.withOpacity(0.1),
+            ),
+          ),
+          widget.child,
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.removeStatusListener((status) {});
+    _controller.dispose();
+    super.dispose();
+  }
+}
